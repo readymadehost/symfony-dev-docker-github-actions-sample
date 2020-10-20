472a2efd3a4a3802671145d30d490b78776169fd
Suson Waiba <susonwaiba@gmail.com>
2020-10-20 16:21:12 +0000

    added test for guest user

---
+++ tests/HomeControllerGuestTest.php
@@ -0,0 +1,25 @@
+<?php
+
+/**
+ * @copyright ReadyMadeHost. All rights reserved.
+ */
+
+namespace App\Tests;
+
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+
+class HomeControllerGuestTest extends WebTestCase
+{
+    public function testHomeForGuest()
+    {
+        $client = self::createClient();
+        /** @var \Symfony\Bundle\FrameworkBundle\Routing\Router $router */
+        $router = self::$container->get('router');
+
+        $client->request('GET', $router->generate('app_home'));
+
+        self::assertResponseIsSuccessful();
+        self::assertSelectorTextContains('h1', 'Symfony dev docker sample');
+        self::assertSelectorTextContains('h4', 'Hello guest!');
+    }
+}

d0294e3c72f8e856bef0d74c35cb9a0027eadd55
Suson Waiba <susonwaiba@gmail.com>
2020-10-20 16:22:09 +0000

    added loggedin test case for home page

---
+++ tests/HomeControllerLoggedinTest.php
@@ -0,0 +1,31 @@
+<?php
+
+/**
+ * @copyright ReadyMadeHost. All rights reserved.
+ */
+
+namespace App\Tests;
+
+use App\Repository\UserRepository;
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+
+class HomeControllerLoggedinTest extends WebTestCase
+{
+    public function testHomeForLoggedin()
+    {
+        $client = self::createClient();
+        /** @var \Symfony\Bundle\FrameworkBundle\Routing\Router $router */
+        $router = self::$container->get('router');
+        /** @var UserRepository $userRepository */
+        $userRepository = self::$container->get(UserRepository::class);
+
+        $loggedinEmail = 'test@example.com';
+        $testUser = $userRepository->findOneBy(['email' => $loggedinEmail]);
+        $client->loginUser($testUser);
+
+        $client->request('GET', $router->generate('app_home'));
+
+        self::assertResponseIsSuccessful();
+        self::assertSelectorTextContains('h4', 'Hello ' . $loggedinEmail . '!');
+    }
+}
